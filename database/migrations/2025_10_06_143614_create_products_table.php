908524daa55405a0c4e64e3b5eeafd3823db2dec
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-10-06 20:39:20 +0600

    products

--- database/migrations/2025_10_06_143614_create_products_table.php
+++ database/migrations/2025_10_06_143614_create_products_table.php
@@ -11,11 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    Schema::create('products', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->text('description')->nullable();
+        $table->decimal('price', 10, 2);
+        $table->integer('stock')->default(0);
+        $table->foreignId('category_id')->constrained()->onDelete('cascade');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
