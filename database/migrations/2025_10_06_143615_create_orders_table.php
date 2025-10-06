c125a7335e81669c7523f0e55140d50fb2e48393
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-10-06 20:39:51 +0600

    orders table

--- database/migrations/2025_10_06_143615_create_orders_table.php
+++ database/migrations/2025_10_06_143615_create_orders_table.php
@@ -11,12 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orders', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
-
+    Schema::create('orders', function (Blueprint $table) {
+        $table->id();
+        $table->string('customer_name');
+        $table->string('customer_email');
+        $table->decimal('total_price', 10, 2);
+        $table->enum('status', ['pending', 'processing', 'completed', 'cancelled'])->default('pending');
+        $table->timestamps();
+    });
+}
     /**
      * Reverse the migrations.
      */
