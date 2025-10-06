417eaf337bf370c15def8437d15d15985d002051
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-10-06 20:41:10 +0600

    orders

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_name',
+        'customer_email',
+        'total_price',
+        'status'
+    ];
 }
