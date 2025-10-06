c3fab5d156c20f51480b24fc6d2bfb941f961c3b
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-10-06 20:42:57 +0600

    order_Controller

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -4,46 +4,35 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class OrderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function index() {
+        return Order::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request) {
+        $request->validate([
+            'customer_name' => 'required',
+            'customer_email' => 'required|email',
+            'total_price' => 'required|numeric',
+        ]);
+        $order = Order::create($request->all());
+        return response()->json($order, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+    public function show(Order $order) {
+        return $order;
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
+    public function update(Request $request, Order $order) {
+        $order->update($request->all());
+        return response()->json($order, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+    public function destroy(Order $order) {
+        $order->delete();
+        return response()->json(null, 204);
     }
 }
